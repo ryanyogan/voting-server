28b1c7672344b7ae736b40458206f9da6cf833e3
Ryan Yogan <ryanyogan@gmail.com>
2015-11-22 11:52:22 -0600

    immutable specs in place, ready to build something

--- test/immutable_spec.js
+++ test/immutable_spec.js
@@ -1,4 +1,5 @@
 import { expect } from 'chai';
+import { List, fromJS, Map } from 'immutable';
 
 describe('Immutability', () => {
   describe('a number', () => {
@@ -12,6 +13,49 @@ describe('Immutability', () => {
 
       expect(nextState).to.equal(43);
       expect(state).to.equal(42);
+    });
+  });
+
+  describe('A List', () => {
+    function addMovie(currentState, movie) {
+      return currentState.push(movie);
+    }
+
+    it('is immutable', () => {
+      let state = List.of('Ants', 'Batman');
+      let nextState = addMovie(state, 'Goodfellas');
+
+      expect(nextState).to.equal(List.of(
+        'Ants',
+        'Batman',
+        'Goodfellas'
+      ));
+
+      expect(state).to.equal(List.of(
+        'Ants',
+        'Batman'
+      ));
+    });
+  });
+
+  describe('A tree', () => {
+    function addMovie(currentState, movie) {
+      return currentState.update('movies', movies => movies.push(movie));
+    }
+
+    it('is immutable', () => {
+      let state = Map({
+        movies: List.of('Ants', 'Batman')
+      });
+      let nextState = addMovie(state, 'Goodfellas');
+
+      expect(nextState).to.equal(fromJS({
+        movies: ['Ants', 'Batman', 'Goodfellas']
+      }));
+
+      expect(state).to.equal(fromJS({
+        movies: ['Ants', 'Batman']
+      }));
     });
   });
 });
